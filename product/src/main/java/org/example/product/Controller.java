185e56bfadd6b69cf21c996ad3429b312d6457be
KateySt <kateystetsenko@gmail.com>
2024-07-07 20:21:41 +0300

    setup

---
+++ product/src/main/java/org/example/product/Controller.java
@@ -0,0 +1,15 @@
+package org.example.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Controller {
+
+    @GetMapping("/message")
+    public String greetingMessage() {
+        return "hello it is me";
+    }
+}
